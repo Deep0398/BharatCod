a0b7f571affbd452c636d0dda948ab2f54f144df
Kuldeep <kuldeepgwstele@gmail.com>
2024-04-26 15:40:08 +0530

    Updated

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import connectDB from "./src/config/db.js"
 import userRouter from "./src/routes/authRoutes.js";
 import adminRouter from "./src/routes/adminRoutes.js";
 import cors from "cors"
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // import passport from "./passport-config.js"
 
 
@@ -28,6 +33,12 @@ app.use(cors({
     origin: '*'
   }));
 app.use(express.json());
+
+app.use('/uploads',express.static(path.join(__dirname, 'src/uploads')))
+console.log('__filename:', __filename);
+console.log('__dirname:', __dirname);
+console.log('Static files directory:', path.join(__dirname, 'src/uploads'));
+
 app.use('/user',userRouter)
 app.use('/admin',adminRouter);
 const port = process.env.port||9000;
